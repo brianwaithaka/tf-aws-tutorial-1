5ad27b63529edbda591d75b1628111081c67ba91
brianwaithaka <brian@waithaka.me>
2023-06-30 13:50:50 -0400

    learnt outputs and variables files

--- main.tf
+++ main.tf
@@ -1,14 +1,43 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "=5.5.0"
+      source  = "hashicorp/aws"
+      version = "=5.5.0"
     }
   }
 }
 
 
 provider "aws" {
-  region = "canada-central"
-  access_key = ""
+  region                   = "ca-central-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "default"
+
+}
+
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = var.subnet_prefix[0].cidr_block
+  availability_zone = "ca-central-1a"
+
+  tags = {
+    Name = var.subnet_prefix[0].name
+  }
+}
+
+resource "aws_subnet" "subnet-2" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = var.subnet_prefix[1].cidr_block
+  availability_zone = "ca-central-1a"
+
+  tags = {
+    Name = var.subnet_prefix[1].name
+  }
 }
